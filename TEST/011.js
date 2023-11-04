8a482ff3cf0b8b155ec394f749a8399e434a2749
sueWavy <sueboi0311@gmail.com>
2023-11-04 22:42:20 +0900

    CS 전공 때문에 시간이 ...

---
+++ TEST/011.js
@@ -0,0 +1,11 @@
+// 문제11 : for를 이용한 기본 활용
+// 1부터 100까지 모두 더하는 Code를 <pass> 부분에 완성하세요. for를 사용해야 합니다.
+
+let s = 1;
+let answer = 1;
+
+for (let i = 1; i <= 100; i++) {
+  answer += i;
+}
+
+console.log(answer);

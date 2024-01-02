b973ed3e92030cc5752ea304b1cba3cca3d21087
sueWavy <sueboi0311@gmail.com>
2024-01-02 22:00:34 +0900

    React : 심플 일기 - 일기 삭제기능

--- React/simple_diary/src/components/DiaryItem.jsx
+++ React/simple_diary/src/components/DiaryItem.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 
-export default function DiaryItem({ author, content, created, emotion, id }) {
+export default function DiaryItem({
+  author,
+  content,
+  created,
+  emotion,
+  id,
+  onDelete,
+}) {
   return (
     <div className="DiaryItem">
       <div className="info">
@@ -11,6 +18,15 @@ export default function DiaryItem({ author, content, created, emotion, id }) {
         <span className="date">{new Date(created).toLocaleString()}</span>
       </div>
       <div className="content">{content}</div>
+      <button
+        onClick={() => {
+          if (window.confirm(`${id}번쟤 일기를 삭제합니까?`)) {
+            onDelete(id);
+          }
+        }}
+      >
+        삭제하기
+      </button>
     </div>
   );
 }

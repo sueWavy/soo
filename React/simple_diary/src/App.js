2d27bc7763f3732510eb8ca81199fa9542724709
sueWavy <sueboi0311@gmail.com>
2024-01-02 21:40:32 +0900

    React : 심플 일기 - 리스트, 아이템

--- React/simple_diary/src/App.js
+++ React/simple_diary/src/App.js
@@ -1,10 +1,43 @@
 import "./App.css";
 import DiaryEditor from "./components/DiaryEditor";
+import DiaryList from "./components/DiaryList";
 
 function App() {
+  const dummyList = [
+    {
+      id: 1,
+      author: "이수광",
+      content: "안녕하쇼~",
+      emotion: 5,
+      created: new Date().getTime(),
+    },
+    {
+      id: 2,
+      author: "이숙끄앙",
+      content: "안녕하쇼~2",
+      emotion: 4,
+      created: new Date().getTime(),
+    },
+    {
+      id: 3,
+      author: "이슈강",
+      content: "안녕하쇼~3",
+      emotion: 2,
+      created: new Date().getTime(),
+    },
+    {
+      id: 4,
+      author: "이수팡",
+      content: "안녕하쇼~4",
+      emotion: 1,
+      created: new Date().getTime(),
+    },
+  ];
+
   return (
     <div className="App">
       <DiaryEditor />
+      <DiaryList diaryList={dummyList} />
     </div>
   );
 }

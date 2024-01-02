d6825a5f5c54afda884e374815a7edbc3ed2353f
sueWavy <sueboi0311@gmail.com>
2024-01-02 22:20:18 +0900

    React : 심플 일기 - 일기 수정기능

--- React/simple_diary/src/components/DiaryItem.jsx
+++ React/simple_diary/src/components/DiaryItem.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
 export default function DiaryItem({
   author,
@@ -7,7 +7,38 @@ export default function DiaryItem({
   emotion,
   id,
   onDelete,
+  onEdit,
 }) {
+  const [isEdit, setIsEdit] = useState(false);
+  const toggleIsEdit = () => setIsEdit(!isEdit);
+
+  const [localContent, setLocalContent] = useState(content);
+
+  const handleQuitEdit = () => {
+    setIsEdit(false);
+    setLocalContent(content);
+  };
+
+  const localContentInput = useRef();
+
+  const handleEdit = () => {
+    if (localContent.length < 5) {
+      localContentInput.current.focus();
+      return;
+    }
+
+    if (window.confirm(`${id}번 일기를 수정합니까?`)) {
+      onEdit(id, localContent);
+      toggleIsEdit();
+    }
+  };
+
+  const handleDelete = () => {
+    if (window.confirm(`${id}번쟤 일기를 삭제합니까?`)) {
+      onDelete(id);
+    }
+  };
+
   return (
     <div className="DiaryItem">
       <div className="info">
@@ -17,16 +48,30 @@ export default function DiaryItem({
         <br />
         <span className="date">{new Date(created).toLocaleString()}</span>
       </div>
-      <div className="content">{content}</div>
-      <button
-        onClick={() => {
-          if (window.confirm(`${id}번쟤 일기를 삭제합니까?`)) {
-            onDelete(id);
-          }
-        }}
-      >
-        삭제하기
-      </button>
+      <div className="content">
+        {isEdit ? (
+          <>
+            <textarea
+              ref={localContentInput}
+              value={localContent}
+              onChange={(e) => setLocalContent(e.target.value)}
+            ></textarea>
+          </>
+        ) : (
+          <>{content}</>
+        )}
+      </div>
+      {isEdit ? (
+        <>
+          <button onClick={handleQuitEdit}>돌아가기</button>
+          <button onClick={handleEdit}>수정완료</button>
+        </>
+      ) : (
+        <>
+          <button onClick={handleDelete}>삭제하기</button>
+          <button onClick={toggleIsEdit}>수정하기</button>
+        </>
+      )}
     </div>
   );
 }
--- React/simple_diary/src/components/DiaryList.jsx
+++ React/simple_diary/src/components/DiaryList.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import DiaryItem from "./DiaryItem";
 
-export default function DiaryList({ diaryList, onDelete }) {
+export default function DiaryList({ diaryList, onDelete, onEdit }) {
   return (
     <div className="DiaryList">
       <h2>일기 리스트</h2>
       <h4>{diaryList.length}개의 일기가 있습니다.</h4>
       <div>
         {diaryList.map((it) => (
-          <DiaryItem key={it.id} {...it} onDelete={onDelete} />
+          <DiaryItem key={it.id} {...it} onDelete={onDelete} onEdit={onEdit} />
         ))}
       </div>
     </div>

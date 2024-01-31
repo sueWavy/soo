3c13aed04969d6c29f0c21756ba07de741683344
sueWavy <sueboi0311@gmail.com>
2024-01-31 18:14:44 +0900

    React : 심플일기 - useMemo 최적화

--- React/simple_diary/src/App.js
+++ React/simple_diary/src/App.js
@@ -1,13 +1,36 @@
-import { useRef, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import "./App.css";
 import DiaryEditor from "./components/DiaryEditor";
 import DiaryList from "./components/DiaryList";
 
 function App() {
+  // 일기 데이터 state 상태관리
   const [data, setData] = useState([]);
 
   const dataId = useRef(0);
 
+  const getData = async () => {
+    const res = await fetch(
+      "https://jsonplaceholder.typicode.com/comments"
+    ).then((res) => res.json());
+
+    const initData = res.slice(0, 20).map((it) => {
+      return {
+        author: it.email,
+        content: it.body,
+        emotion: Math.floor(Math.random() * 5) + 1,
+        created_date: new Date().getTime(),
+        id: dataId.current++,
+      };
+    });
+    setData(initData);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  /** 일기 생성 기능 */
   const onCreate = (author, content, emotion) => {
     const created = new Date().getTime();
     const newItem = {
@@ -21,12 +44,14 @@ function App() {
     setData([newItem, ...data]);
   };
 
+  /** 일기 삭제 기능 */
   const onDelete = (targetId) => {
     console.log(`${targetId}가 삭제되었습니다.`);
     const newDiaryList = data.filter((it) => it.id !== targetId);
     setData(newDiaryList);
   };
 
+  /** 일기 수정 기능 */
   const onEdit = (targetId, newContent) => {
     setData(
       data.map((it) =>
@@ -35,9 +60,27 @@ function App() {
     );
   };
 
+  const getDiaryAnalysis = useMemo(() => {
+    console.log("일기 분석 시작");
+
+    const goodCount = data.filter((it) => it.emotion >= 3).length;
+    const badCount = data.length - goodCount;
+    const goodRatio = Math.round((goodCount / data.length) * 100);
+    return { goodCount, badCount, goodRatio };
+  }, [data.length]);
+
+  const { goodCount, badCount, goodRatio } = getDiaryAnalysis;
+
   return (
     <div className="App">
       <DiaryEditor onCreate={onCreate} />
+      <div style={{ textAlign: "center" }}>
+        <br />
+        <div>전체 일기 : {data.length}</div>
+        <div>좋은 일기 : {goodCount}</div>
+        <div>나쁜 일기 : {badCount}</div>
+        <div>좋은 비율 : {goodRatio}%</div>
+      </div>
       <DiaryList diaryList={data} onDelete={onDelete} onEdit={onEdit} />
     </div>
   );

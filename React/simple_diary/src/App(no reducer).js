69b5139ac261567dbdb02cf885040b7ac0cacef5
sueWavy <sueboi0311@gmail.com>
2024-02-02 19:31:30 +0900

    React : useReducer 학습

--- React/simple_diary/src/App(no reducer).js
+++ React/simple_diary/src/App(no reducer).js
@@ -1,36 +1,19 @@
-import { useCallback, useEffect, useMemo, useReducer, useRef } from "react";
+import {
+  useCallback,
+  useEffect,
+  useMemo,
+  useReducer,
+  useRef,
+  useState,
+} from "react";
 import "./App.css";
 import DiaryEditor from "./components/DiaryEditor";
 import DiaryList from "./components/DiaryList";
 // import OptimizeTest from "./OptimizeTest";
 
-/** 기능 함수들을 reducer로 밖에 정리 */
-const reducer = (state, action) => {
-  switch (action.type) {
-    case "INIT": {
-      return action.data;
-    }
-    case "CREATE": {
-      const created_date = new Date().getTime();
-      const newItem = { ...action.data, created_date };
-      return [newItem, ...state];
-    }
-    case "DELETE": {
-      return state.filter((it) => it.id !== action.targetId);
-    }
-    case "EDIT": {
-      return state.map((it) =>
-        it.id === action.targetId ? { ...it, content: action.newContent } : it
-      );
-    }
-    default:
-      return state;
-  }
-};
-
 function App() {
-  // useReducer로 변경
-  const [data, dispatch] = useReducer(reducer, []);
+  // 일기 데이터 state 상태관리
+  const [data, setData] = useState([]);
 
   const dataId = useRef(0);
 
@@ -48,7 +31,7 @@ function App() {
         id: dataId.current++,
       };
     });
-    dispatch({ type: "INIT", data: initData });
+    setData(initData);
   };
 
   useEffect(() => {
@@ -57,21 +40,34 @@ function App() {
 
   /** 일기 생성 기능 */
   const onCreate = useCallback((author, content, emotion) => {
-    dispatch({
-      type: "CREATE",
-      data: { author, content, emotion, id: dataId.current },
-    });
+    const created = new Date().getTime();
+    const newItem = {
+      author,
+      content,
+      emotion,
+      created,
+      id: dataId.current,
+    };
     dataId.current += 1;
+    setData((data) => [newItem, ...data]);
+    // 함수형 업데이트 : 화살표 함수 (인자로 data) setState 상태변화 함수에 함수를 전달하는 걸 함수형 업데이트라고한다.
+    // -> 의존형배열을 비워놓을 수 있게된다.
   }, []);
+  // 왜 useMemo가 아니라 useCallback인가? memo는 값을 반환하고 useCallback은 그 콜백 함수를 반환한다. (차이점 알기)
 
   /** 일기 삭제 기능 */
   const onDelete = useCallback((targetId) => {
-    dispatch({ type: "DELETE", targetId });
+    // const newDiaryList = data.filter((it) => it.id !== targetId); // 전달을 함수형으로 변경 (함수형 업데이트로)
+    setData((data) => data.filter((it) => it.id !== targetId));
   }, []);
 
   /** 일기 수정 기능 */
   const onEdit = useCallback((targetId, newContent) => {
-    dispatch({ type: "EDIT", targetId, newContent });
+    setData((data) =>
+      data.map((it) =>
+        it.id === targetId ? { ...it, content: newContent } : it
+      )
+    );
   }, []);
 
   /** useMemo : 최적화 기능 (답을 기억했다가 바로 적는다 = 같은 답일 경우 리랜더링 X) */
